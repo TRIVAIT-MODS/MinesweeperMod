2b6d38c6a5fd5589fe6d4db55728ccd71b1f4229
TRIVAIT <dimitmapper@gmail.com>
2026-02-14 07:39:54 +0300

    version 2.2.0

--- src/main/java/org/trivait/minesweeper/mixin/TitleScreenMixin.java
+++ src/main/java/org/trivait/minesweeper/mixin/TitleScreenMixin.java
@@ -1,6 +1,5 @@
 package org.trivait.minesweeper.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.TextIconButtonWidget;
@@ -10,7 +9,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.trivait.minesweeper.MinesweeperModClient;
+import org.trivait.minesweeper.config.Config;
 import org.trivait.minesweeper.screen.MinesweeperScreen;
 
 @Mixin(TitleScreen.class)
@@ -21,14 +21,20 @@ public abstract class TitleScreenMixin extends Screen {
 
     @Inject(method = "init", at = @At("TAIL"))
     private void addMinesweeperButton(CallbackInfo ci) {
+        Config cfg = MinesweeperModClient.getConfig();
+        if (cfg.mainMenuButtonPosition == null) {
+            cfg.mainMenuButtonPosition = Config.MainMenuButtonPosition.RIGHT_MULTIPLAYER;
+        }
         TextIconButtonWidget minesweeperBtn = TextIconButtonWidget.builder(
                 Text.empty(),
-                (button) -> this.client.setScreen(new MinesweeperScreen(this.title)),
+                (button) -> this.client.setScreen(new MinesweeperScreen()),
                 true
         ).width(20).texture(Identifier.of("minesweeper", "icon/button"), 16, 16).build();
 
-        // ставим кнопку справа от стандартных
-        minesweeperBtn.setPosition(this.width / 2 + 104, this.height / 4 + 48);
+        minesweeperBtn.setPosition(
+                cfg.mainMenuButtonPosition.getX(this.width),
+                cfg.mainMenuButtonPosition.getY(this.height)
+        );
         this.addDrawableChild(minesweeperBtn);
     }
 }
