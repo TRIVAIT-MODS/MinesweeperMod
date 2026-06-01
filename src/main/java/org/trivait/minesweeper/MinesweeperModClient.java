b9d5fb0892c359dcdae660f972dd4d60e3d5fc14
TRIVAIT <dimitmapper@gmail.com>
2026-06-01 22:00:35 +0400

    Version 3.0.0 for 1.21.5

--- src/main/java/org/trivait/minesweeper/MinesweeperModClient.java
+++ src/main/java/org/trivait/minesweeper/MinesweeperModClient.java
@@ -1,63 +1,24 @@
 package org.trivait.minesweeper;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import org.trivait.minesweeper.config.Config;
-import org.trivait.minesweeper.config.ConfigManager;
+import org.trivait.minesweeper.game.SavedGame;
 
 public class MinesweeperModClient implements ClientModInitializer {
 
     public static final String MOD_ID = "minesweeper";
-    private static Config config;
-
-    public static final class SavedGame {
-        public int w;
-        public int h;
-        public int mines;
-
-        public boolean minesPlaced;
-        public int remainingSafe;
-
-        public boolean alive;
-        public boolean won;
-        public boolean firstClick;
-
-        public boolean timerRunning;
-        public int elapsedSeconds;
-
-        public boolean[] mine;
-        public boolean[] revealed;
-        public boolean[] flagged;
-        public int[] adjacent;
-        public float[] revealProgress;
-        public int[] delayTicks;
-        public boolean[] scheduled;
-    }
+    public static Config CONFIG;
 
     private static SavedGame savedGame;
 
     @Override
     public void onInitializeClient() {
-        config = ConfigManager.loadOrCreate();
+        AutoConfig.register(Config.class, GsonConfigSerializer::new);
+        CONFIG = AutoConfig.getConfigHolder(Config.class).getConfig();
     }
 
-    public static Config getConfig() {
-        return config;
-    }
-
-    public static void saveConfig() {
-        ConfigManager.save(config);
-    }
-
-    public static void incrementWins() {
-        config.wins++;
-        saveConfig();
-    }
-
-    public static SavedGame getSavedGame() {
-        return savedGame;
-    }
-
-    public static void setSavedGame(SavedGame game) {
-        savedGame = game;
-    }
+    public static SavedGame getSavedGame() { return savedGame; }
+    public static void setSavedGame(SavedGame game) { savedGame = game; }
 }
