d26e89a48da077ccf0c5a07aebcb0b2436608720
TRIVAIT <dimitmapper@gmail.com>
2026-06-01 21:47:55 +0400

    Version 3.0.0 for 1.21.2

---
+++ src/main/java/org/trivait/minesweeper/game/Cell.java
@@ -0,0 +1,11 @@
+package org.trivait.minesweeper.game;
+
+public class Cell {
+    public boolean mine;
+    public boolean revealed;
+    public boolean flagged;
+    public int adjacent;
+    public float revealProgress = -1f;
+    public int delayTicks = -1;
+    public boolean scheduled = false;
+}
---
+++ src/main/java/org/trivait/minesweeper/screen/ScoreSelectWidget.java
@@ -0,0 +1,78 @@
+package org.trivait.minesweeper.screen;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.trivait.minesweeper.MineSweeperMod;
+
+public class ScoreSelectWidget extends ClickableWidget {
+    private float scale = 1.0f;
+    private float targetScale = 1.0f;
+    private final float speed = 0.30f;
+
+    private final Screen parent;
+
+    public ScoreSelectWidget(int x, int y, int width, int height, Screen parent) {
+        super(x, y, width, height, Text.empty());
+        this.parent = parent;
+    }
+
+    @Override
+    protected void renderWidget(DrawContext ctx, int mouseX, int mouseY, float delta) {
+        MatrixStack matrixStack = ctx.getMatrices();
+
+        if (this.isHovered()) {
+            targetScale = 1.05f;
+        } else {
+            targetScale = 1.0f;
+        }
+
+        scale += (targetScale - scale) * speed;
+
+        int cx = getX() + getWidth() / 2;
+        int cy = getY() + getHeight() / 2;
+
+        matrixStack.push();
+        matrixStack.translate(cx, cy, 0);
+        matrixStack.scale(scale, scale, 0);
+        matrixStack.translate(-cx, -cy, 0);
+
+        ctx.fill(getX(), getY(), getX()+width, getY()+2, -2);
+        ctx.fill(getX(), getY()+height, getX()+width, getY()+height-2, -1);
+        ctx.fill(getX(), getY(), getX()+2, getY()+height-2, -2);
+        ctx.fill(getX()+width, getY(), getX()+width-2, getY()+height-2, -1);
+
+        ctx.drawTexture(RenderLayer::getGuiTextured, Identifier.of(MineSweeperMod.MOD_ID, "textures/gui/score.png"), getX() + 1, getY() + 1, 0, 0, width - 2, height - 2, width - 2, height - 2);
+
+        var mc = net.minecraft.client.MinecraftClient.getInstance();
+        net.minecraft.text.MutableText label = net.minecraft.text.Text.translatable("leaderboard.mode.score").styled(s -> s.withBold(true));
+        int tw = mc.textRenderer.getWidth(label) * 2;
+        int tx = getX() + (width - tw) / 2;
+        int ty = getY() + height - mc.textRenderer.fontHeight * 2 - 8;
+        matrixStack.push();
+        matrixStack.translate(tx, ty, 0);
+        matrixStack.scale(2f, 2f, 1f);
+        ctx.drawText(mc.textRenderer, label, 0, 0, -1, true);
+        matrixStack.pop();
+
+        matrixStack.pop();
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        MinecraftClient.getInstance().setScreen(new ScoreLeaderboardScreen(parent));
+
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
+
+    }
+}
