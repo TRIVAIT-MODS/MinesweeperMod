d0962a6ddc7bb328fe33a96015f476ad6718eae3
TRIVAIT <dimitmapper@gmail.com>
2026-06-01 22:36:19 +0400

    Version 3.0.0 for 1.21.11

---
+++ src/main/java/org/trivait/minesweeper/config/GameMode.java
@@ -0,0 +1,7 @@
+package org.trivait.minesweeper.config;
+
+public enum GameMode {
+    DEFAULT,
+    LEADERBOARD_WIN_COUNT,
+    LEADERBOARD_TIME
+}
---
+++ src/main/java/org/trivait/minesweeper/screen/ScoreLeaderboardScreen.java
@@ -0,0 +1,96 @@
+package org.trivait.minesweeper.screen;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.input.KeyInput;
+import net.minecraft.text.Text;
+import org.trivait.minesweeper.MinesweeperModClient;
+import org.trivait.minesweeper.config.GameMode;
+import org.trivait.minesweeper.leaderboard.BoardCategory;
+import org.trivait.minesweeper.leaderboard.LeaderboardCache;
+
+public class ScoreLeaderboardScreen extends Screen {
+    private final Screen parent;
+
+    private LeaderboardWidget leaderboard;
+    private BoardCategory boardCategory = BoardCategory.S8x8;
+    private LeaderboardCache CACHE = new LeaderboardCache();
+
+    private ButtonWidget c8x8Button;
+    private ButtonWidget c16x16Button;
+    private ButtonWidget c26x18Button;
+
+    private ButtonWidget playButton;
+
+    public ScoreLeaderboardScreen(Screen parent) {
+        super(Text.empty());
+        this.parent = parent;
+    }
+
+    @Override
+    protected void init() {
+        this.leaderboard = new LeaderboardWidget(10, 10, width/2+125, height-20, CACHE, GameMode.LEADERBOARD_WIN_COUNT, boardCategory);
+
+        this.c8x8Button = ButtonWidget.builder(Text.literal("8x8"), b -> {
+            boardCategory = BoardCategory.S8x8;
+            leaderboard.setCategory(boardCategory);
+            b.active = false;
+            c16x16Button.active = true;
+            c26x18Button.active = true;
+        }).dimensions(width/2+((width/2-15)/2), height/2-10-10-20-20, 100, 20).build();
+        this.addDrawableChild(c8x8Button);
+
+        c8x8Button.active=false;
+
+        this.c16x16Button = ButtonWidget.builder(Text.literal("16x16"), b -> {
+            boardCategory = BoardCategory.S16x16;
+            leaderboard.setCategory(boardCategory);
+            b.active = false;
+            c8x8Button.active = true;
+            c26x18Button.active = true;
+        }).dimensions(width/2+((width/2-15)/2), height/2-10-20, 100, 20).build();
+        this.addDrawableChild(c16x16Button);
+
+        this.c26x18Button = ButtonWidget.builder(Text.literal("26x18"), b -> {
+            boardCategory = BoardCategory.S26x18;
+            leaderboard.setCategory(boardCategory);
+            b.active = false;
+            c16x16Button.active = true;
+            c8x8Button.active = true;
+        }).dimensions(width/2+((width/2-15)/2), height/2, 100, 20).build();
+        this.addDrawableChild(c26x18Button);
+
+        this.playButton = ButtonWidget.builder(Text.translatable("leaderboard.play"), button -> {
+            client.setScreen(new LeaderboardMinesweeperScreen(
+                    boardCategory.toGameSettings(),
+                    MinesweeperModClient.CONFIG.enableAnimations,
+                    GameMode.LEADERBOARD_WIN_COUNT,
+                    boardCategory
+            ));
+        }).dimensions(width/2+((width/2-15)/2), height/2+30, 100, 20).build();
+        this.addDrawableChild(playButton);
+
+        this.addDrawableChild(leaderboard);
+        refresh();
+    }
+
+    @Override
+    public boolean keyPressed(KeyInput input) {
+        if (input.key() == 294) {
+            refresh();
+        }
+
+        return super.keyPressed(input);
+    }
+
+    private void refresh() {
+        CACHE.invalidate(GameMode.LEADERBOARD_WIN_COUNT);
+        CACHE.refreshIfNeeded(GameMode.LEADERBOARD_WIN_COUNT, () -> {});
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        this.client.setScreen(parent);
+    }
+}
